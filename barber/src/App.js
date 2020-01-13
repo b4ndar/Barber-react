6f98d9ce6c5a516f770e772eeb7fd6b62788218e
صفوان القليطي <safwanalqulayti@gmail.com>
2020-01-13 11:21:46 +0300

    test

--- barber/src/App.js
+++ barber/src/App.js
@@ -7,11 +7,7 @@ import Services from './Services'
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-  
-=======
       <Services></Services>
->>>>>>> f1bc1ee2b6b317dc71d307304c236d03bb7326c4
     </div>
   );
 }
